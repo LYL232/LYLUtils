02a9f6aa74855e17d16d5ec985c6ecd88ddfcbd8
林勇良 <3183246942@qq.com>
2019-10-03 11:41:43 +0800

    删除不需要上传的文件夹

--- cmake-build-debug/CMakeFiles/LYL.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/LYL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/LYL.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LYL.
-# This may be replaced when dependencies are built.
