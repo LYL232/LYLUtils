9f0d9f8dbdc484f4f2975590cfbeb2fa562eacf8
林勇良 <3183246942@qq.com>
2019-10-10 21:13:55 +0800

    实现舞蹈链

---
+++ cpp/DanceLink/DanceLink.h
@@ -0,0 +1,174 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedStructInspection"
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#ifndef LYL_UTILS_DANCELINK_H
+#define LYL_UTILS_DANCELINK_H
+
+#include<cstring>
+#include<algorithm>
+#include <vector>
+
+
+class DanceLinkX {
+    typedef std::pair<size_t, size_t> point;
+public:
+    /**
+     * 需指定最大处理规模, 最大处理规模等于矩阵元素个数加上列数, 即:
+     * 对于n行m列矩阵, max_process_size = n*m + m
+     * @param _max_process_size
+     */
+    explicit DanceLinkX(size_t _max_process_size) {
+        max_process_size = _max_process_size + 1;
+        nodes = (Node *) malloc(max_process_size * sizeof(Node));
+        head = (int *) malloc(max_process_size * sizeof(int));
+        num_of_nodes_of_col = (size_t *) malloc(max_process_size * sizeof(int));
+        selected = (size_t *) malloc(max_process_size * sizeof(size_t));
+    };
+
+    ~DanceLinkX() {
+        free(nodes);
+        free(head);
+        free(num_of_nodes_of_col);
+        free(selected);
+    }
+
+    std::vector<size_t> dance(size_t rows, size_t cols, const std::vector<point> &sparse) {
+        init(cols);
+        for (auto i : sparse) {
+            link(i.first, i.second);
+        }
+        found = false;
+        _dance(0);
+        std::vector<size_t> res;
+        if (found) {
+            for (register size_t i = 0; i < num_of_selected; i++) {
+                res.push_back(selected[i]);
+            }
+        }
+        return res;
+    }
+
+private:
+    size_t max_process_size, cnt, num_of_selected;
+    struct Node {
+        //每个点的左右上下指针，所在行列
+        int l, r, u, d, col, row;
+    } *nodes;
+    int *head;//每行的头结点
+    bool found;
+    size_t *num_of_nodes_of_col,//每列的节点数
+            *selected;//选了哪几行
+
+    /**
+     * 搜索答案
+     * @param deep 搜索深度
+     */
+    void _dance(size_t deep) {
+        if (nodes[0].r == 0) {
+            num_of_selected = deep;
+            found = true;
+            return;
+        }
+        register int i, j, c = nodes[0].r;
+        for (i = nodes[0].r; i != 0; i = nodes[i].r) {
+            if (num_of_nodes_of_col[i] < num_of_nodes_of_col[c]) {
+                c = i;
+            }
+        }
+        rm_col(c);
+        for (i = nodes[c].d; i != c; i = nodes[i].d) {
+            selected[deep] = (size_t) nodes[i].row;
+            for (j = nodes[i].r; j != i; j = nodes[j].r) {
+                rm_col(nodes[j].col);
+            }
+            _dance(deep + 1);
+            if (found) {
+                return;
+            }
+            for (j = nodes[i].l; j != i; j = nodes[j].l) {
+                resume(nodes[j].col);
+            }
+        }
+        resume(c);
+    }
+
+    /**
+     * 往 _row行_col列加入值为1的点
+     * @param _row
+     * @param _col
+     */
+    void link(size_t _row, size_t _col) {
+        num_of_nodes_of_col[_col]++;
+        nodes[cnt].row = _row;
+        nodes[cnt].col = _col;
+        nodes[cnt].u = _col;
+        nodes[cnt].d = nodes[_col].d;
+        nodes[nodes[_col].d].u = cnt;
+        nodes[_col].d = cnt;
+        if (head[_row] < 0) {
+            head[_row] = nodes[cnt].r = nodes[cnt].l = cnt;
+        } else {
+            nodes[cnt].r = (size_t) head[_row];
+            nodes[cnt].l = nodes[head[_row]].l;
+            nodes[nodes[head[_row]].l].r = cnt;
+            nodes[head[_row]].l = cnt;
+        }
+        cnt++;
+    }
+
+    /**
+     * 为插入num个元素做初始化
+     * num 等于需要处理的矩阵的列数
+     * @param num
+     */
+    void init(size_t num) {
+        found = false;
+        num_of_selected = 0;
+        for (register size_t i = 0; i <= num; i++) {
+            nodes[i].r = i + 1;
+            nodes[i].l = i - 1;
+            nodes[i].u = nodes[i].d = i;
+        }
+        nodes[num].r = 0;
+        nodes[0].l = num;
+        memset(head, -1, max_process_size * sizeof(int));
+        memset(num_of_nodes_of_col, 0, max_process_size * sizeof(int));
+        cnt = num + 1;
+    }
+
+    /**
+     * 删除涉及_col列的集合
+     * @param _col
+     */
+    void rm_col(int _col) {
+        nodes[nodes[_col].l].r = nodes[_col].r, nodes[nodes[_col].r].l = nodes[_col].l;
+        for (int i = nodes[_col].d; i != _col; i = nodes[i].d) {
+            for (int j = nodes[i].r; j != i; j = nodes[j].r) {
+                nodes[nodes[j].d].u = nodes[j].u;
+                nodes[nodes[j].u].d = nodes[j].d;
+                num_of_nodes_of_col[nodes[j].col]--;
+            }
+        }
+    }
+
+    /**
+     * 恢复涉及_col列的集合
+     * @param _col
+     */
+    void resume(int _col) {
+        for (int i = nodes[_col].u; i != _col; i = nodes[i].u) {
+            for (int j = nodes[i].l; j != i; j = nodes[j].l) {
+                nodes[nodes[j].d].u = j;
+                nodes[nodes[j].u].d = j;
+                num_of_nodes_of_col[nodes[j].col]++;
+            }
+        }
+        nodes[nodes[_col].l].r = _col;
+        nodes[nodes[_col].r].l = _col;
+    }
+
+};
+
+#endif //LYL_UTILS_DANCELINK_H
+
+#pragma clang diagnostic pop
---
+++ cpp/DanceLink/demo.cpp
@@ -0,0 +1,46 @@
+/**
+ * 使用示例
+ * 判题地址:https://www.luogu.org/problem/P4929
+ * 题目描述
+ * 给定一个N行M列的矩阵，矩阵中每个元素要么是1，要么是0
+ * 你需要在矩阵中挑选出若干行，使得对于矩阵的每一列j，在你挑选的这些行中，有且仅有一行的第j个元素为1
+ * 输入格式
+ * 第一行两个数N,M
+ * 接下来N行，每行M个数字0或1，表示这个矩阵
+ * 输出格式
+ * 一行输出若干个数表示答案，两个数之间用空格隔开，输出任一可行方案均可，顺序随意
+ * 若无解，输出“No Solution!”（不包含引号）
+ */
+#include "../IOUtils/IOUtils.h"
+#include "DanceLink.h"
+
+using namespace std;
+const int MAX_N = 250525;
+int n, m;
+
+int main() {
+    DanceLinkX danceLink(MAX_N);
+    n = IOUtils::read();
+    m = IOUtils::read();
+    register size_t i, j;
+    int f;
+    std::vector<pair<size_t, size_t> > sparse;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
+            f = IOUtils::read();
+            if (f) {
+                sparse.emplace_back(i, j);
+            }
+        }
+    }
+    vector<size_t> ans = danceLink.dance((size_t) n, (size_t) m, sparse);
+    if (ans.empty()) {
+        printf("No Solution!");
+    } else {
+        for (auto each : ans) {
+            printf("%u ", each);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
