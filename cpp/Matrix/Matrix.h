e16958af2d1555e1ff6e81b7fa06ef879f362341
林勇良 <3183246942@qq.com>
2019-10-16 23:02:10 +0800

    实现基本矩阵表示

---
+++ cpp/Matrix/Matrix.h
@@ -0,0 +1,90 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#pragma ide diagnostic ignored "OCUnusedStructInspection"
+#ifndef LYL_UTILS_MATRIX_H
+#define LYL_UTILS_MATRIX_H
+
+#include <malloc.h>
+#include <cstring>
+
+/**
+ * 简单的矩阵表示
+ * @tparam NUM_TYPE
+ */
+template<typename NUM_TYPE>
+class Matrix {
+public:
+    NUM_TYPE **elements;
+
+    Matrix() {
+        elements = nullptr;
+        row = col = 0;
+    }
+
+    /**
+     *  初始化, 下标从0开始
+     * @param _rows 行数
+     * @param _cols 列数
+     */
+    Matrix(size_t _rows, size_t _cols) {
+        row = _rows;
+        col = _cols;
+        apply_mem();
+    }
+
+    Matrix(const Matrix &matrix) {
+        row = matrix.row;
+        col = matrix.col;
+        apply_mem();
+        const size_t col_size = sizeof(NUM_TYPE) * col;
+        for (register size_t i = 0; i < row; ++i) {
+            memcpy(elements[i], matrix.elements[i], col_size);
+        }
+    }
+
+    ~Matrix() {
+        free_mem();
+    }
+
+    /**
+     * 重新调整矩阵的大小
+     * @param _rows 行数
+     * @param _cols 列数
+     * @param set_zero 是否置0
+     */
+    void resize(size_t _rows, size_t _cols, bool set_zero = false) {
+        free_mem();
+        row = _rows;
+        col = _cols;
+        apply_mem();
+        if (set_zero) {
+            const size_t col_size = col * sizeof(NUM_TYPE);
+            for (register size_t i = 0; i < row; ++i) {
+                memset(elements[i], 0, col_size);
+            }
+        }
+    }
+
+private:
+    size_t row, col;
+
+    void apply_mem() {
+        elements = (NUM_TYPE **) malloc(row * sizeof(NUM_TYPE *));
+        const size_t col_size = col * sizeof(NUM_TYPE);
+        for (register size_t i = 0; i < row; ++i) {
+            elements[i] = (NUM_TYPE *) malloc(col_size);
+        }
+    }
+
+    void free_mem() {
+        for (register size_t i = 0; i < row; ++i) {
+            free(elements[i]);
+        }
+        free(elements);
+    }
+
+};
+
+#endif //LYL_UTILS_MATRIX_H
+
+#pragma clang diagnostic pop
---
+++ cpp/Matrix/SparseMatrix.h
@@ -0,0 +1,189 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#pragma ide diagnostic ignored "OCUnusedStructInspection"
+#ifndef LYL_UTILS_SPARSEMATRIX_H
+#define LYL_UTILS_SPARSEMATRIX_H
+
+#include <vector>
+#include "../Iterator/BaseIterator.h"
+
+
+/**
+ * 简单版本的稀疏矩阵,目前只作简单的表示工具,没有任何检查和优化
+ * @tparam NUM_TYPE
+ */
+template<typename NUM_TYPE>
+class SparseMatrix {
+public:
+
+    struct PointValue {
+        size_t row, col;
+        NUM_TYPE value;
+
+        PointValue() {
+            row = col = 0;
+            value = 0;
+        }
+
+        PointValue(size_t _row, size_t _col, NUM_TYPE _value) {
+            row = _row;
+            col = _col;
+            value = _value;
+        }
+
+        PointValue(const PointValue &p) {
+            row = p.row;
+            col = p.col;
+            value = p.value;
+        }
+    };
+
+    class Iterator {
+    public:
+
+        Iterator(const Iterator &iterator) : matrix(iterator.matrix) {
+            index = iterator.index;
+        }
+
+        inline PointValue operator*() const {
+            return get();
+        }
+
+        inline bool operator!=(const Iterator &other) const {
+            return index != other.index || (&matrix != &(other.matrix));
+        }
+
+        inline const Iterator &operator++() {
+            ++index;
+            return *this;
+        }
+
+        inline void to_next() {
+            if (++index >= matrix.elements.size()) {
+                index = matrix.elements.size();
+                throw IteratorException(IteratorException::TypeCode::OutOfBound);
+            };
+        }
+
+        inline void to_previous() {
+            if (--index < 0) {
+                index = 0;
+                throw IteratorException(IteratorException::TypeCode::OutOfBound);
+            }
+        }
+
+        inline PointValue get() const {
+            if (index < 0 || index >= matrix.elements.size()) {
+                throw IteratorException(IteratorException::TypeCode::InvalidIndex);
+            }
+            return matrix.elements[index];
+        }
+
+        inline bool has_next() const {
+            return index < matrix.elements.size() - 1;
+        }
+
+        inline bool has_previous() const {
+            return index > 0;
+        }
+
+    private:
+        friend class SparseMatrix;
+
+        size_t index;
+        const SparseMatrix &matrix;
+
+        explicit Iterator(const SparseMatrix &_matrix, size_t _index) : matrix(_matrix) {
+            index = _index;
+        }
+    };
+
+
+    /**
+     *  开始迭代的点
+     * @return
+     */
+    Iterator begin() const {
+        return Iterator(*this, 0);
+    }
+
+    /**
+     *  结束的迭代的点
+     * @return
+     */
+    Iterator end() const {
+        return Iterator(*this, elements.size());
+    }
+
+    SparseMatrix() {
+        rows = cols = 0;
+    };
+
+    /**
+     *  初始化, 指定矩阵行数和列数
+     * @param _rows 行数
+     * @param _cols 列数
+     */
+    SparseMatrix(size_t _rows, size_t _cols) {
+        rows = _rows;
+        cols = _cols;
+    }
+
+    SparseMatrix(const SparseMatrix &sparseMatrix) {
+        rows = sparseMatrix.rows;
+        cols = sparseMatrix.cols;
+        elements = sparseMatrix.elements;
+    }
+
+    inline void set_rows(size_t _rows) {
+        rows = _rows;
+    }
+
+    inline void set_cols(size_t _cols) {
+        cols = _cols;
+    }
+
+    inline size_t get_rows() const {
+        return rows;
+    }
+
+    inline size_t get_cols() const {
+        return cols;
+    }
+
+    /**
+     * 设置指定点为存在数
+     * @param _row
+     * @param _col
+     */
+    inline void set(size_t _row, size_t _col, NUM_TYPE _value) {
+        rows = _row > rows ? _row : rows;
+        cols = _col > cols ? _col : cols;
+        elements.emplace_back(_row, _col, _value);
+    }
+
+    /**
+     * 清空所有的已存在的点
+     */
+    inline void clear() {
+        elements.clear();
+    }
+
+    /**
+     * 返回所有的元素列表
+     * @return 所有的元素列表
+     */
+    inline const std::vector<PointValue> get_elements() const {
+        return elements;
+    }
+
+protected:
+    // 行数和列数
+    size_t rows, cols;
+    //稀疏矩阵非0元素的点
+    std::vector<PointValue> elements;
+};
+
+#endif //LYL_UTILS_SPARSEMATRIX_H
+
+#pragma clang diagnostic pop
